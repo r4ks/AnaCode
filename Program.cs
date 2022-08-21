536e7f798c913f42740730cc1390062fed1ba893
r1ck5 <ricardosasaki1@gmail.com>
2022-08-21 12:29:27 +0300

    initial

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using static System.Console;
+
+public class Program
+{
+    private static void Main(string[] args)
+    {
+        const string programText =
+@"using System;
+using System.Collections;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(""Hello, World!"");
+        }
+    }
+}";
+
+        // Console.WriteLine("Hello, World!");
+
+        SyntaxTree tree = CSharpSyntaxTree.ParseText(programText);
+        CompilationUnitSyntax root = tree.GetCompilationUnitRoot();
+
+        WriteLine($"The tree is a {root.Kind()} node.");
+        WriteLine($"The tree has {root.Members.Count} elements in it.");
+        WriteLine($"The tree has {root.Usings.Count} using statements. They are:");
+        foreach (UsingDirectiveSyntax element in root.Usings)
+            WriteLine($"\t{element.Name}");
+    }
+}
